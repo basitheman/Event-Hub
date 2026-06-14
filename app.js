8a7dba6483e9a803fce1a0a06a6dd31d62d7c825
moh <amjamal4050@gmail.com>
2026-06-15 02:43:20 +0300

    Adding express dependence and setup layout ejs

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
+const session = require('express-session');
+const methodOverride = require('method-override');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
-app.get('/', (req, res) => res.send('EventHub API'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
+
 
 app.listen(3000, () => console.log('Server on 3000'));
