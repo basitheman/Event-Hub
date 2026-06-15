c5b0fd0499f8c30d7e09835ae21f7698b60d427e
moh <amjamal4050@gmail.com>
2026-06-15 03:01:08 +0300

    Completing layout for nav and footer and cleaner app.js

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const methodOverride = require('method-override');
 const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 const app = express();
+
+const homeRouter = require('./routes/home');
+const eventsRouter = require('./routes/events');
+const registrationsRouter = require('./routes/registrations');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 app.set('layout', 'layout');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'eventhub-secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 2 * 60 * 60 * 1000 }
+}));
 
 
-app.listen(3000, () => console.log('Server on 3000'));
+app.use('/', homeRouter);
+app.use('/events', eventsRouter);
+app.use('/registrations', registrationsRouter);
+
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Server error: ' + err.message);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`\n🎟  EventHub running at http://localhost:${PORT}\n`);
+});
